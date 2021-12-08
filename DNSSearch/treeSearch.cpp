f94a3cf0607478b8885011f018e982a3d651067b
xiaozhu-sorce <790362227@qq.com>
2021-12-08 18:47:37 +0800

    complete DNSSearch

--- DNSSearch/treeSearch.cpp
+++ DNSSearch/treeSearch.cpp
@@ -1,96 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 
-typedef struct app {
-	char a[20];
-	struct app *left;
-	struct app *right;
-	char b[20];
-} app;
-int cout = 0;
+//树的基本数据结构
+typedef struct Node {
+	char a[20];//存放域名
+	char IP[20];//根结点存放IP
+	struct Node *left;
+	struct Node *right;
+} Node;
+
+int count = 0;
 
 //数据保存在相同目录下的text.txt文件下;
-app *jianshu(app *bt, FILE *fp) {
+Node *createTree(Node *bt, FILE *fp) {
 	char a[20];
-	if (fscanf(fp, "%s ", &a) != EOF) {
+	//从fp打开的文件中读取格式化输入
+	if (fscanf(fp, "%s ",a) != EOF) {
+		//base case
 		if (a[0] == '#')
 			bt = NULL;
 		else {
-			bt = (app *)malloc(sizeof(app));
+			bt = (Node *)malloc(sizeof(Node));
+
+			//存放域名
 			strcpy(bt->a, a);
+
+			//判读是否为叶子结点 --- 也就是判断域名开始是否为www,bbs,ftp
 			if (strcmp(a, "www") == 0 || strcmp(a, "bbs") == 0 || strcmp(a, "ftp") == 0 ) {
-				fscanf(fp, "%s ", &a);
-				strcpy(bt->b, a);
+				fscanf(fp, "%s ", a);
+				strcpy(bt->IP, a);
 			}
-			bt->left = jianshu(bt->left, fp);
-			bt->right = jianshu(bt->right, fp);
+
+			//后序遍历递归构造树
+			bt->left = createTree(bt->left, fp);
+			bt->right = createTree(bt->right, fp);
 		}
 	}
 	return bt;
 }
 
-app *tianjia(app *bt, char fenge[4][20], int i, char ip[20]) {
-	/*puts(fenge[i]);
-	puts(bt->a);
-	printf("%d", i);*/
-	if (bt != NULL && strcmp(bt->a, fenge[i]) == 0) {
+Node *addIP(Node *bt, char splitStr[4][20], int i, char ip[20]) {
+	if (bt != NULL && strcmp(bt->a, splitStr[i]) == 0) {
 		if (i == 0) {
-			puts(bt->b);
+			puts(bt->IP);
 			return bt;
 		}
-		bt->left = tianjia(bt->left, fenge, --i, ip);
+		bt->left = addIP(bt->left, splitStr, --i, ip);
 	} else {
 		if (bt == NULL) {
 			if (i == -1) {
 				return bt;
 			}
-			bt = (app *)malloc(sizeof(app));
+
+			bt = (Node *)malloc(sizeof(Node));
 			bt->right = NULL;
 			bt->left = NULL;
-			strcpy(bt->a, fenge[i]);
-			if (strcmp(fenge[i], "www") == 0 || strcmp(fenge[i], "bbs") == 0 || strcmp(fenge[i], "ftp") == 0 ) {
-				strcpy(bt->b, ip);
+
+			strcpy(bt->a, splitStr[i]);
+
+			if (strcmp(splitStr[i], "www") == 0 || strcmp(splitStr[i], "bbs") == 0 || strcmp(splitStr[i], "ftp") == 0 ) {
+				strcpy(bt->IP, ip);
 			}
-			bt->left = tianjia(bt->left, fenge, --i, ip);
+			bt->left = addIP(bt->left, splitStr, --i, ip);
 		} else
-			bt->right = tianjia(bt->right, fenge, i, ip);
+			bt->right = addIP(bt->right, splitStr, i, ip);
 	}
 	return bt;
 }
 
-void qianxu(app *bt, FILE *fp) {
+//将写好的数据写入文件
+void renew(Node *bt, FILE *fp) {
 	if (bt == NULL) {
 		fprintf(fp, "# ");
 	} else {
 		fprintf(fp, "%s ", bt->a);
 		if (strcmp(bt->a, "www") == 0 || strcmp(bt->a, "bbs") == 0 || strcmp(bt->a, "ftp") == 0 ) {
-			fprintf(fp, "%s ", bt->b);
+			fprintf(fp, "%s ", bt->IP);
 		}
-		qianxu(bt->left, fp);
-		qianxu(bt->right, fp);
+		renew(bt->left, fp);
+		renew(bt->right, fp);
 	}
 }
 
-void jiediangeshu(app *bt, char fenge[4][20], int i) {
-	/*for (i; i >= 0; i--) {
-		puts(fenge[i]);
-	}*/
-	//puts(bt->a);
-	//puts(fenge[i]);
-	if (bt != NULL && strcmp(bt->a, fenge[i]) == 0) {
+//i为有几个分割出来的子字符串
+void serchIP(Node *bt, char splitStr[4][20], int i) {
+
+	if (bt != NULL && strcmp(bt->a, splitStr[i]) == 0) {
 		if (i == 0) {
-			puts(bt->b);
+			puts(bt->IP);
 			return;
 		}
-		jiediangeshu(bt->left, fenge, --i);
+		serchIP(bt->left, splitStr, --i);
 
 	} else {
 		if (bt == NULL) {
-			printf("找不到服务器或发生 DNS 错误");
+			cout << "找不到服务器或发生 DNS 错误" << endl;
 			return;
 		}
-		jiediangeshu(bt->right, fenge, i);
+		serchIP(bt->right, splitStr, i);
 	}
 }
 
@@ -99,58 +109,66 @@ int main() {
 	char str[80];
 	char ip[20];
 	char *token;
-	char ju = 'y';
-	int xuan;
-	char fenge[4][20];
-	int flag = 0;
-	app *bt;
+	char ifNext = 'y';
+	int selection;
+	char splitStr[4][20];
+
+	Node *bt;
 	FILE *fp1 = NULL;
 	FILE *fp2 = NULL;
 	fp1 = fopen("text.txt", "r");
-	bt = jianshu(bt, fp1);
+	bt = createTree(bt, fp1);
 	fp2 = fopen("text.txt", "w");
-	//qianxu(bt);
-	while (ju != 'n') {
-		printf("请选择功能：1，增加网址；2，查询网址;3,退出系统\n");
-		scanf("%d", &xuan);
+
+	//renew(bt);
+	while (ifNext != 'n') {
+		cout << "请选择功能：1:增加网址; 2:查询网址; 3:退出系统" << endl;
+		cin >> selection;
 		getchar();
-		if (xuan == 3) {
-			qianxu(bt, fp2);
+
+		if (selection == 3) {
+			renew(bt, fp2);
 			exit(0);
 		}
+
 		printf("请输入地址：");
-		scanf("%s", &str);
+		cin >> str;
 		getchar();
+
 		int i = 0;
+		//将输入的字符串进行分解，分解字符串 str 为一组字符串，"."为分隔符。
 		token = strtok(str, ".");
+
 		while (token != NULL) {
-			strcpy(fenge[i], token);
+			strcpy(splitStr[i], token);
 			i++;
+			//继续获得分割的str的下一个子字符串
 			token = strtok(NULL, ".");
 		}
-		switch (xuan) {
+
+		switch (selection) {
 			case 1:
-				printf("请输入ip：");
-				scanf("%s", &ip);
+				cout << "请输入ip：";
+				cin >> ip;
 				getchar();
-				bt = tianjia(bt, fenge, --i, ip);
+				bt = addIP(bt, splitStr, --i, ip);
 				break;
 			case 2:
-				jiediangeshu(bt, fenge, --i);
+				serchIP(bt, splitStr, --i);
 				break;
 		}
 		printf("是否继续？y/n\n");
-		scanf("%c", &ju);
-		//qianxu(bt, fp2);
+		cin >> ifNext;
 		getchar();
 	}
-	qianxu(bt, fp2);
+	
+	renew(bt, fp2);
 	fclose(fp2);
 	fclose(fp1);
 	printf("谢谢使用");
 }
 /*
-测试数据
+可查询的数据如下所示：
 www.baidu.com 220.181.27.5
 www.google.com 66.249.89.104
 www.microsoft.com 207.46.20.60
@@ -166,5 +184,5 @@ www.tsinghua.edu.cn 166.111.4.100
 ftp.tsinghua.edu.cn 166.111.8.229
 www.beijing.gov.cn 210.73.64.10
 www.shanghai.gov.cn 61.129.65.58
+www.xiaozhu-sorce.github.io 123.123.123
 */
-
